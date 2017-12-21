2ddd68819e2a3b6730f4737acdefaf10d7093cf5
Homer Quan <homer@Homers-MacBook-Pro.local>
2017-12-21 13:22:56 -0500

    a quick commit

--- src/backend/config/graphql.js
+++ src/backend/config/graphql.js
@@ -1,19 +1,19 @@
-import config from "../config/environment";
 import expressGraphQL from "express-graphql";
-import { graphiqlExpress } from 'graphql-server-express';
-import bodyParser from 'body-parser';
+import { graphiqlExpress } from "graphql-server-express";
+import bodyParser from "body-parser";
 import { PubSub } from "graphql-subscriptions";
 import { SubscriptionServer } from "subscriptions-transport-ws";
 import { execute, subscribe } from "graphql";
 import { makeExecutableSchema } from "graphql-tools";
 import resolvers from "../graphql/resolvers";
 import typeDefs from "../graphql/schema";
+import jwt from 'jsonwebtoken';
+import config from './environment';
 
-const colors = require('colors');
+const colors = require("colors");
 
-
-const init = (app,server) => {
-	//Setup GRAPHQL 	
+const init = (app, server) => {
+	//Setup GRAPHQL
 	// make schema executable
 	const schema = makeExecutableSchema({
 		typeDefs,
@@ -33,13 +33,17 @@ const init = (app,server) => {
 	app.use(config.graphqlPath, graphqlMiddleware);
 	console.log(`GraphQL Server is now running on ${config.graphqlPath}`.green);
 
-
 	// Add graphiql dev console
-	app.use('/graphiql', bodyParser.json(), graphiqlExpress({
-  		endpointURL: config.graphqlPath,
-  		subscriptionsEndpoint: 'ws://localhost:3003'+config.graphqlSubscriptionsPath,
-	}));
-    
+	app.use(
+		"/graphiql",
+		bodyParser.json(),
+		graphiqlExpress({
+			endpointURL: config.graphqlPath,
+			subscriptionsEndpoint:
+				"ws://localhost:3003" + config.graphqlSubscriptionsPath
+		})
+	);
+
 	// GraphQL subscription
 	// create subscription server
 	new SubscriptionServer(
@@ -53,7 +57,11 @@ const init = (app,server) => {
 			path: config.graphqlSubscriptionsPath
 		}
 	);
-	console.log(`GraphQL Subscriptions are now running on ${config.graphqlSubscriptionsPath}`.green);
-}
+	console.log(
+		`GraphQL Subscriptions are now running on ${
+			config.graphqlSubscriptionsPath
+		}`.green
+	);
+};
 
-export {init}
+export { init };
