b9cffd3a83d87c1a9ec53491c2c2e11e71101350
Homer <homerquan@gmail.com>
2017-12-30 02:02:39 -0500

    a regular commit

--- src/backend/config/graphql.js
+++ src/backend/config/graphql.js
@@ -40,7 +40,7 @@ const init = (app, server) => {
 		graphiqlExpress({
 			endpointURL: config.graphqlPath,
 			subscriptionsEndpoint:
-				"ws://console-api.stage.reflen.com/" + config.graphqlSubscriptionsPath
+				"ws://console-api.stage.reflen.com" + config.graphqlSubscriptionsPath
 		})
 	);
 
