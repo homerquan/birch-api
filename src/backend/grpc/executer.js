4ffad1c88bfa25cb6036869e1241effb77724f9e
Homer <homerquan@gmail.com>
2018-01-06 23:51:10 -0500

    a regular commit

--- src/backend/grpc/executer.js
+++ src/backend/grpc/executer.js
@@ -2,19 +2,19 @@
 * @Author: Homer
 * @Date:   2018-01-03 22:01:31
 * @Last Modified by:   Homer
-* @Last Modified time: 2018-01-06 22:57:35
+* @Last Modified time: 2018-01-06 23:50:38
 */
 
-const ListBotsHandler = require('./handlers/ListBotsHandler')
+const ListBotsHandler = require('./handlers/SuggestResponseHandler')
 
 const getCode = (code) => {
-	return require('../../config/constants').get('codes',code);
+	return require('../config/constants').get('codes',code);
 }
 
 //TODO: for large data using mongo stream
 
 module.exports = (request,cb) => {
 	if(request.typeCode == getCode('SUGGEST_RESPONSE')) {
-		ListBotsHandler(request,cb)
+		SuggestResponseHandler(request,cb)
 	}
 }
