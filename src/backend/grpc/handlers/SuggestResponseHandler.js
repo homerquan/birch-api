046b73fac5cdc490ef00b626cc82f89fed663797
Homer <homerquan@gmail.com>
2018-01-07 07:13:44 -0500

    a regular commit

---
+++ src/backend/grpc/handlers/SuggestResponseHandler.js
@@ -0,0 +1,10 @@
+/*
+* @Author: Homer
+* @Date:   2018-01-04 00:23:03
+* @Last Modified by:   Homer
+* @Last Modified time: 2018-01-06 23:51:00
+*/
+
+module.exports = (request,cb) => {
+	console.log(request);
+};
