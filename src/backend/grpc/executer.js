987bb077560f2f44d4783e5396517f64a207216a
Homer <homerquan@gmail.com>
2018-01-06 23:53:38 -0500

    a regular commit

--- src/backend/grpc/executer.js
+++ src/backend/grpc/executer.js
@@ -2,10 +2,10 @@
 * @Author: Homer
 * @Date:   2018-01-03 22:01:31
 * @Last Modified by:   Homer
-* @Last Modified time: 2018-01-06 23:50:38
+* @Last Modified time: 2018-01-06 23:53:19
 */
 
-const ListBotsHandler = require('./handlers/SuggestResponseHandler')
+const SuggestResponseHandler = require('./handlers/SuggestResponseHandler')
 
 const getCode = (code) => {
 	return require('../config/constants').get('codes',code);
