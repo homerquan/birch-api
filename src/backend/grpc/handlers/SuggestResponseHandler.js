84c64477495193e0f5e51eca5907c40a3e56dc76
Homer <homerquan@gmail.com>
2018-01-07 13:42:13 -0500

    a regular commit

--- src/backend/grpc/handlers/SuggestResponseHandler.js
+++ src/backend/grpc/handlers/SuggestResponseHandler.js
@@ -2,9 +2,12 @@
 * @Author: Homer
 * @Date:   2018-01-04 00:23:03
 * @Last Modified by:   Homer
-* @Last Modified time: 2018-01-06 23:51:00
+* @Last Modified time: 2018-01-07 13:41:34
 */
+
+import { pubsub } from "../../graphql/subscriptions"; 
 
 module.exports = (request,cb) => {
 	console.log(request);
+	cb(null, { data: "ok" });
 };
