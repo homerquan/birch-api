54a4a5ef7966f92b3b5bd6c1fccde16db7589867
Homer Quan <homer@bigmacs-MacBook-Pro.local>
2019-05-23 03:19:18 -0400

    a regular commit

---
+++ src/middlewares/index.js
@@ -0,0 +1,10 @@
+
+//Translate JWT to User (id and role), overwrite variables
+const jwtToUser = (req, res, next) => {
+  console.log(req.body);
+  next();
+};
+
+var middleware = [jwtToUser];
+
+module.exports = middleware;
