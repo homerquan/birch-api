1d7d9362a67c8dc46d6c11e42724625688a53318
Homer Quan <homer@bigmacs-MacBook-Pro.local>
2019-06-08 12:12:53 -0400

    a regular commit

---
+++ src/graphql/schema/test.js
@@ -0,0 +1,20 @@
+/*
+ * @Author: homer
+ * @Date:   2019-05-09 14:13:33
+ * @Last Modified by:   homer
+ * @Last Modified time: 2019-05-09 17:35:52
+ */
+import pubsub from '../pubsub';
+
+export const load = schemaComposer => {
+  // add subscription here
+  schemaComposer.Subscription.addFields({
+    test: {
+      type: 'String',
+      resolve: payload => {
+        return payload;
+      },
+      subscribe: () => pubsub.asyncIterator('test'),
+    },
+  });
+};
